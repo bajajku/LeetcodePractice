cc084294e42b9bb963d6078cfc935605430887b5
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-01-09 18:43:25 -0500

    Create linkedListCycle.py
    track visited.

---
+++ LinkedList/linkedListCycle.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+        visited = set()
+
+        while head != None:
+            if(head in visited):
+                return True
+            visited.add(head)
+            head = head.next
+        
+        return False
+        
+        
