8c6ad65028c6fdaa7fc467d5d92201165b6dac91
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-01-10 18:15:08 -0500

    Create searchInRotatedSort.py
    Same Intuition:
    
    Minimum one half will always be sorted.
    if middle is target return middle
    
    1) If we find left is sorted check if target is in left ... middle
        else: eliminate left
    2) If right is sorted check if target is in right .... middle:
        else: eliminate right

---
+++ BinarySearch/searchInRotatedSort.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) -1
+
+        while l <= r:
+
+            m = (l+r) // 2
+
+            if(nums[m]) == target:
+                return m
+            
+            if(nums[l] <= nums[m]):
+                if(target >= nums[l] and target < nums[m]):
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if(target <= nums[r] and nums[m] < target):
+                    l = m + 1
+                else:
+                    r = m - 1
+        
+        return -1
