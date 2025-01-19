8503064a292c22a22ba26760e51d9fee7561019b
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-01-19 17:41:31 -0500

    Update BTBSS2.py
    added bottom up as welllll

--- DynamicProgramming/BTBSS2.py
+++ DynamicProgramming/BTBSS2.py
@@ -29,6 +29,26 @@ class Solution(object):
         
         return (function(0, 1))
 
+    # Bottom Up
+    memo = [[0] * 2 for _ in range(len(prices) + 1)]
+    
+    for i in range(len(prices) - 1, -1, -1):
+
+        for j in [0, 1]:
+            res = 0
+            if(j == 1):
+                b = memo[i + 1][0] - prices[i]
+                nB = memo[i + 1][1] + 0
+                res =  max(b, nB)
+            elif(j == 0):
+                s = memo[i + 1][1] + prices[i]
+                nS = memo[i + 1][0]
+                res = max(s, nS)
+            
+            memo[i][j] = res
+    
+    return memo[0][1]
+
 
 
             
