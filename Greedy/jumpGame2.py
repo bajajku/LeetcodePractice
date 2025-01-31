13df7a499a880c6499211cb3e07a1543f591b9bb
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-01-31 12:57:45 -0500

    Update jumpGame2.py
    Added greedy solution:
    
    FOllows a BFS approach to check if we can reach a certain level from a level. And if the level contain last element we are done.

--- Greedy/jumpGame2.py
+++ Greedy/jumpGame2.py
@@ -22,17 +22,34 @@ class Solution(object):
         #     return minI
         # return(function(0))
 
-        memo = [0] * len(nums)
+        # memo = [0] * len(nums)
 
-        for i in range(len(nums)-2, -1, -1):
-            minI = float("inf")
-            for j in range(1, nums[i] + 1):
-                val = float("inf")
-                if(i + j <= len(nums) -1):
-                    val = 1 + memo[i + j]
-                minI = min(minI, val)
-            memo[i] = minI
-        return memo[0]
+        # for i in range(len(nums)-2, -1, -1):
+        #     minI = float("inf")
+        #     for j in range(1, nums[i] + 1):
+        #         val = float("inf")
+        #         if(i + j <= len(nums) -1):
+        #             val = 1 + memo[i + j]
+        #         minI = min(minI, val)
+        #     memo[i] = minI
+        # return memo[0]
+'''
+Greedy Solution:
+'''
+        res = 0
+        l, r = 0, 0
+
+        while r < len(nums) -1:
+
+            farthest = 0
+            for i in range(l, r +1):
+                farthest = max(farthest, i + nums[i])
+
+            l = r + 1
+            r = farthest
+            res += 1
+        
+        return(res)
 
 
 
