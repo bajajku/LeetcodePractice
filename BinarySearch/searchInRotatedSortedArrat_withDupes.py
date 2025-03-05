59be0299543f646ba004e674793ded08fa39c5cf
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-03-05 02:41:42 -0500

    Create searchInRotatedSortedArrat_withDupes.py

---
+++ BinarySearch/searchInRotatedSortedArrat_withDupes.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = (l + r) // 2
+            if(nums[m] == target):
+                return True
+
+            if(nums[l] == nums[m] == nums[r]):
+                l += 1
+                r -= 1
+                continue
+            
+            elif(nums[l] <= nums[m]):
+                if(nums[l] <= target < nums[m]):
+                    r = m - 1
+                else:
+                    l = m + 1
+            
+            else:
+                if(nums[m] < target <= nums[r]):
+                    l = m + 1
+                else:
+                    r = m -1
+            
+        
+        return False
