9b6931ef51e7b3c478b332726a2ee6e5c2343004
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-05-20 17:30:45 -0400

    Update nQueens.py
    updated the solution and added modularized code using conventions and better readability.

--- Backtracking/nQueens.py
+++ Backtracking/nQueens.py
@@ -1,66 +1,56 @@
-class Solution(object):
-    def solveNQueens(self, n):
-        """
-        :type n: int
-        :rtype: List[List[str]]
-        """
+class Solution:
+    '''
+    00  01  02  03
+    10  11  12  13
+    20  21  22  23
+    30  31  32  33
+    
+    
+    '''
 
-        '''three things:
+    def __init__(self):
+        self.cols = set()
+        self.left_diagonal = set()
+        self.right_diagonal = set()
+        self.result = []
+    
+    def buildState(self, n):
+        state = [["."] * n for _ in range(n)]
+        return state
         
-            -> Check in same row, 
-            -> check in same col
-            -> check in same diagonal
+    def isValid(self, row, col):
+        if col not in self.cols and (row + col) not in self.left_diagonal and (col - row) not in self.right_diagonal:
+            return True
+        return False
+
+
+    def update(self, row, col, command):
+        if command == "insert":
+            self.cols.add(col)
+            self.left_diagonal.add(row + col)
+            self.right_diagonal.add(col - row)
+        elif command == "delete":
+            self.cols.remove(col)
+            self.left_diagonal.remove(row + col)
+            self.right_diagonal.remove(col - row)
+    
+    def backtrack(self, r, state, n):
+        if r == n:
+            self.result.append(["".join(row) for row in state])  # deep copy in string format
+            return
         
-        '''
+        for c in range(n):
+            if self.isValid(r, c):
+                state[r][c] = "Q"
+                self.update(r, c, "insert")
 
-        ROWS, COLS = n, n
+                self.backtrack(r + 1, state, n)
 
-        rows = set()        
-        cols = set()
-
-        leftDiagonal = set()
-        rightDiagonal = set()
-
-        res = []
-
-        board = [["."] * COLS for _ in range(ROWS)]
-
-        def isValid(r, c):
-            return (
-                r not in rows and
-                c not in cols and
-                (r-c) not in leftDiagonal and
-                (r+c) not in rightDiagonal
-            )
-
+                state[r][c] = "."
+                self.update(r, c, "delete")
         
-        def function(r):
-
-            if(r == ROWS):
-                copy = ["".join(row) for row in board]
-                res.append(copy[:])
-                return
-            
-            for c in range(COLS):
-
-                if(isValid(r, c)):
-
-                    board[r][c] = "Q"
-
-                    rows.add(r)
-                    cols.add(c)
-                    leftDiagonal.add(r-c)
-                    rightDiagonal.add(r+c)
-
-                    function(r + 1)
-
-                    rows.remove(r)
-                    cols.remove(c)
-                    leftDiagonal.remove(r-c)
-                    rightDiagonal.remove(r+c)
-
-                    board[r][c] = "."
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        state = self.buildState(n)
+        self.backtrack(0, state, n)
+        return(self.result)
         
-        function(0)
-
-        return res
