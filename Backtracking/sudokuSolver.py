956e3eac4a19b626d927c833977748ae209ebe75
Kunal Bajaj <123124459+bajajku@users.noreply.github.com>
2025-05-21 12:29:40 -0400

    Update sudokuSolver.py
    Updated solution with modularized code :))

--- Backtracking/sudokuSolver.py
+++ Backtracking/sudokuSolver.py
@@ -1,71 +1,66 @@
-class Solution(object):
-    def solveSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
-        ROWS, COLS = 9, 9
+class Solution:
+
+    def __init__(self):
+        self.rows = [set() for _ in range(9)]
+        self.cols = [set() for _ in range(9)]
+        self.sub_boxes = [set() for _ in range(9)]
     
-        # Initialize sets to track numbers in rows, columns, and squares
-        rows = {i: set() for i in range(ROWS)}
-        cols = {i: set() for i in range(COLS)}
-        squares = {(r, c): set() for r in range(3) for c in range(3)}
+    def initialState(self, board):
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                ele = board[r][c]
+                if ele != ".":
+                    self.rows[r].add(ele)
+                    self.cols[c].add(ele)
+                    self.sub_boxes[(r // 3) * 3 + (c // 3)].add(ele)
         
-        # Fill the sets with existing numbers on the board
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] != ".":
-                    val = board[r][c]
-                    rows[r].add(val)
-                    cols[c].add(val)
-                    squares[(r // 3, c // 3)].add(val)
-        
-        # Helper function to check if a value is valid in the current cell
-        def isValid(row, col, val):
-            return (
-                val not in rows[row] and
-                val not in cols[col] and
-                val not in squares[(row // 3, col // 3)]
-            )
-        
-        # Backtracking function
-        def backtrack(r, c):
-            if r == ROWS:  # Reached the end of the board
-                return True
-            if c == COLS:  # Move to the next row
-                return backtrack(r + 1, 0)
-            if board[r][c] != ".":  # Skip filled cells
-                return backtrack(r, c + 1)
-            
-            for num in map(str, range(1, 10)):  # Try numbers 1 to 9
-                if isValid(r, c, num):
-                    # Place the number and update sets
-                    board[r][c] = num
-                    rows[r].add(num)
-                    cols[c].add(num)
-                    squares[(r // 3, c // 3)].add(num)
-                    
-                    # Recurse to the next cell
-                    if backtrack(r, c + 1):
-                        return True
-                    
-                    # Backtrack: Remove the number
-                    board[r][c] = "."
-                    rows[r].remove(num)
-                    cols[c].remove(num)
-                    squares[(r // 3, c // 3)].remove(num)
-            
-            return False  # No valid number found for this cell
-        
-    # Start backtracking from the top-left corner
-        backtrack(0, 0)
-        
-        return board
+    def isValid(self, num, r, c):
+        if num not in self.rows[r] and num not in self.cols[c] and num not in self.sub_boxes[(r // 3) * 3 + (c // 3)]:
+            return True
+        return False
 
+    def update(self, num, r, c, command):
+        if command == "insert":
+            self.rows[r].add(num)
+            self.cols[c].add(num)
+            self.sub_boxes[(r // 3) * 3 + (c // 3)].add(num)
+        
+        elif command == "delete":
+            self.rows[r].remove(num)
+            self.cols[c].remove(num)
+            self.sub_boxes[(r // 3) * 3 + (c // 3)].remove(num)
 
+    def backtrack(self, r, c, board):
 
+        if r == 9:
+            return True
+        
+        if c == 9:
+            return self.backtrack(r + 1, 0, board)
+        
+        if board[r][c] != ".":
+            return self.backtrack(r, c + 1, board)
+        
+        for num in range(1, 10):
+            num = str(num)
 
+            if self.isValid(num, r, c):
+
+                board[r][c] = num
+                self.update(num, r, c, "insert")
                 
+                if self.backtrack(r, c + 1, board):
+                    return True
+                
+                self.update(num, r, c, "delete")
+                board[r][c] = "."
 
+    
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        self.initialState(board)
 
-        
+        self.backtrack(0, 0, board)
+
